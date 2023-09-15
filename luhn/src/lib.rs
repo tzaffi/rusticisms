647adb7d6c24ce4cc40111d43b98cad55d649374
Zeph Grunschlag <zephaniah@zibby.com>
2023-09-15 13:29:54 -0500

    luhn passing

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,41 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    let mut invalid = false;
+    let digitize = |c: char| -> Option<u32> {
+        match c {
+            ' ' => None,
+            _ => {
+                let d = c.to_digit(10);
+                if d.is_none() {
+                    invalid = true;
+                }
+                d
+            }
+        }
+    };
+
+    let digits = code.chars().filter_map(digitize).collect::<Vec<u32>>();
+    if invalid || digits.len() <= 1 {
+        return false;
+    }
+
+    digits
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(summand)
+        .sum::<u32>()
+        % 10
+        == 0
+}
+
+fn summand(i_d: (usize, u32)) -> u32 {
+    let (i, d) = i_d;
+    match i % 2 {
+        0 => d,
+        _ => {
+            let e = 2 * d;
+            e / 10 + e % 10
+        }
+    }
 }
